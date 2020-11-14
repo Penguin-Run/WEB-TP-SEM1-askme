3a097c14ed22db56a3bbc42534966be92a716dfe
Penguin-Run <penguinlgb@gmail.com>
2020-11-14 20:15:09 +0300

    template pagination added

--- app/views.py
+++ app/views.py
@@ -15,7 +15,7 @@ sample_question_answers = [
 	{
 		'id': idx,
 		'text': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the...',
-	} for idx in range(9)
+	} for idx in range(7)
 ]
 
 questions = [
@@ -24,68 +24,55 @@ questions = [
 		'title': f'Best question? #{idx}',
 		'text': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the...',
 		# выбирает случайный тэг из dict tags. !!! dict tags дублируется здесь и в context processors!! TODO: исправить
+		# явялется числом хотя должен формировать массив. TODO:Исправить!!
 		'tags': random.choice(list(range(1, 6))), 
 		'likes': 5,
 		'answers': sample_question_answers,
 	} for idx in range(NUMBER_OF_QUESTIONS)
 ]
 
+def paginate(request, object_list, per_page=3):
+    paginator = Paginator(object_list, per_page)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return page_obj
+
 
 def new_questions(request, pk = 1):
-	question_pages = Paginator(questions, 4)
-	# TODO: обработка случая pk > page number
+	question_pages = paginate(request, questions)
 
-	pages_num = []
-	for i in range(1, question_pages.num_pages + 1):
-		pages_num.append(i)
 	return render(request, 'index.html', {
-        'questions': question_pages.page(pk),
-        'pages': pages_num
+        'page_obj': question_pages,
     })
 
 def hot_questions(request, pk = 1):
 	hot_questions = questions
-	question_pages = Paginator(hot_questions, 4)
-	# TODO: обработка случая pk > page number
+	question_pages = paginate(request, hot_questions)
 
-	pages_num = []
-	for i in range(1, question_pages.num_pages + 1):
-		pages_num.append(i)
 	return render(request, 'hot_questions.html', {
-        'questions': question_pages.page(pk),
-        'pages': pages_num
+        'page_obj': question_pages,
     })
 
-def tag_questions(request, string, pk = 1):
+def tag_questions(request, string):
 	questions_for_this_tag = []
 	for i in range(NUMBER_OF_QUESTIONS):
 		if tags.get(questions[i]['tags']) == string:
 			questions_for_this_tag.append(questions[i])
 
-	question_pages = Paginator(questions_for_this_tag, 4)
-	# TODO: обработка случая pk > page number
+	page_obj = paginate(request, questions_for_this_tag)
 
-	pages_num = []
-	for i in range(1, question_pages.num_pages + 1):
-		pages_num.append(i)
 	return render(request, 'tag_questions.html', {
-        'questions': question_pages.page(pk),
-        'pages': pages_num,
+        'page_obj': page_obj,
         'tag': string,
     })
 
 def question_answers(request, question_id, pk = 1):
 	question = questions[question_id]
 
-	answers_pages = Paginator(question['answers'], 4)
-
-	pages_num = []
-	for i in range(1, answers_pages.num_pages + 1):
-		pages_num.append(i)
+	page_obj = paginate(request, question['answers'])
 	return render(request, 'answers_page.html', {
+		'page_obj': page_obj,
 		'question': question,
-		'answers_pages': answers_pages.page(pk),
-		'pages': pages_num,
 		})
 
 def ask_question(request):
