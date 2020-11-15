9c8d44b02321f2fbd87ded04b9044a9faaa5cb6b
Penguin-Run <penguinlgb@gmail.com>
2020-11-15 04:23:17 +0300

    db startes

--- app/models.py
+++ app/models.py
@@ -16,39 +16,39 @@ class User(models.Model):
 
 
 class Question(models.Model):
-	titile = models.CharField(max_length = 256, verbose_name = 'Заголовок')
+	title = models.CharField(max_length = 256, verbose_name = 'Заголовок')
 	text = models.TextField(verbose_name = 'Текст')
-	date_create = models.DateField(verbose_name = 'Дата создания')
+	date_create = models.DateField(auto_now_add=True, verbose_name = 'Дата создания')
 	rating = models.IntegerField(verbose_name = 'Рейтинг')
 	author = models.ForeignKey('User', on_delete = models.CASCADE, verbose_name = 'Автор')
 	tags = models.ManyToManyField('Tag', verbose_name = 'Тэги')
 	
 	def __str__(self):
-        return self.title
+		return self.title
 
-    class Meta:
-        verbose_name = 'Вопрос'
-        verbose_name_plural = 'Вопросы'
+	class Meta:
+		verbose_name = 'Вопрос'
+		verbose_name_plural = 'Вопросы'
 
 
 class Answer(models.Model):
 	text = models.TextField(verbose_name = 'Текст')
 	rating = models.IntegerField(verbose_name = 'Рейтинг')
-	date_create = models.DateField(verbose_name = 'Дата создания')
+	date_create = models.DateField(auto_now_add=True, verbose_name = 'Дата создания')
 	is_correct = models.BooleanField(default=False, verbose_name = 'Правильность')
 	question = models.ForeignKey('Question', on_delete = models.CASCADE, verbose_name = 'Вопрос')
 	author = models.ForeignKey('User', on_delete = models.CASCADE, verbose_name = 'Автор')
 
 	def __str__(self):
-        return self.text
+		return self.text
 
-    class Meta:
-        verbose_name = 'Ответ'
-        verbose_name_plural = 'Ответы'
+	class Meta:
+		verbose_name = 'Ответ'
+		verbose_name_plural = 'Ответы'
 
 
 class Tag(models.Model):
-	name = models.CharField(max_length = 256, verbode_name = 'Название тэга')
+	name = models.CharField(max_length = 256, verbose_name = 'Название тэга')
 
 	def __str__(self):
 		return self.name
